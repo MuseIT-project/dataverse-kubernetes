c90e816b6fa9443f4ed2338638593ba8bb9c0629
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2022-10-18 17:02:10 +0200

    upload script adjusted

--- test/test_upload.sh
+++ test/test_upload.sh
@@ -1,7 +1,7 @@
 export API_TOKEN=5cbd0d29-6174-4c44-9285-f865c9820eba
-export SERVER_URL=http://dataverse:8080
+export SERVER_URL=http://0.0.0.0:8080
 export DATAVERSE_ID=root
 export PERSISTENT_IDENTIFIER=doi:10.34622/datarepositorium/SGXCQO
-export FILE=example.json
+export FILE=./test/example.json
 
 curl -H X-Dataverse-key:$API_TOKEN -X POST "$SERVER_URL/api/dataverses/$DATAVERSE_ID/datasets/:import?pid=$PERSISTENT_IDENTIFIER&release=yes" --upload-file $FILE
