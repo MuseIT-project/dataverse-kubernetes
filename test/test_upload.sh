dc02410e79e8f0a2b13082716d7ca406bd7627bb
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2022-10-18 17:15:19 +0200

    Publish root dataverse

--- test/test_upload.sh
+++ test/test_upload.sh
@@ -4,4 +4,5 @@ export DATAVERSE_ID=root
 export PERSISTENT_IDENTIFIER=doi:10.34622/datarepositorium/SGXCQO
 export FILE=./test/example.json
 
+curl -H X-Dataverse-key:$API_TOKEN -X POST $SERVER_URL/api/dataverses/$DATAVERSE_ID/actions/:publish
 curl -H X-Dataverse-key:$API_TOKEN -X POST "$SERVER_URL/api/dataverses/$DATAVERSE_ID/datasets/:import?pid=$PERSISTENT_IDENTIFIER&release=yes" --upload-file $FILE
