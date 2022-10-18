fa6fa61679d62aaa2dd7ecc28a0185b32f38e98f
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2022-10-18 16:10:27 +0200

    default token changed

--- personas/docker-compose/secrets/db_sample.sh
+++ personas/docker-compose/secrets/db_sample.sh
@@ -1,1 +1,2 @@
-export PGPASSWORD=`cat /secrets/db/password`; psql -U dataverse dataverse -h postgres -c "select * from dvobject;"
+export PGPASSWORD=`cat /secrets/db/password`; psql -U dataverse dataverse -h postgres -c "update apitoken set tokenstring='5cbd0d29-6174-4c44-9285-f865c9820eba' where id=1"
+export PGPASSWORD=`cat /secrets/db/password`; psql -U dataverse dataverse -h postgres -c "select * from apitoken"
