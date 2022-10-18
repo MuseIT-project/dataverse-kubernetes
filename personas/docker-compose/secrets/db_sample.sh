63aaabb522dcf10ef3b80b1eff8d7fdb4bc1afcb
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2022-10-18 17:11:09 +0200

    Superuser SQL to import test dataset

--- personas/docker-compose/secrets/db_sample.sh
+++ personas/docker-compose/secrets/db_sample.sh
@@ -1,2 +1,3 @@
 export PGPASSWORD=`cat /secrets/db/password`; psql -U dataverse dataverse -h postgres -c "update apitoken set tokenstring='5cbd0d29-6174-4c44-9285-f865c9820eba' where id=1"
+export PGPASSWORD=`cat /secrets/db/password`; psql -U dataverse dataverse -h postgres -c "update authenticateduser set superuser=True where id=1"
 export PGPASSWORD=`cat /secrets/db/password`; psql -U dataverse dataverse -h postgres -c "select * from apitoken"
