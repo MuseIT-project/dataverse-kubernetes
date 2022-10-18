085469be9223121a8c6093be9e6d7f5eb224922c
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2022-10-18 17:26:40 +0200

    Enable OAI-PMH server

--- personas/docker-compose/secrets/db_sample.sh
+++ personas/docker-compose/secrets/db_sample.sh
@@ -1,3 +1,4 @@
+export PGPASSWORD=`cat /secrets/db/password`; psql -U dataverse dataverse -h postgres -c "insert into setting (content, name) values (True, ':OAIServerEnabled')"
 export PGPASSWORD=`cat /secrets/db/password`; psql -U dataverse dataverse -h postgres -c "update apitoken set tokenstring='5cbd0d29-6174-4c44-9285-f865c9820eba' where id=1"
 export PGPASSWORD=`cat /secrets/db/password`; psql -U dataverse dataverse -h postgres -c "update authenticateduser set superuser=True where id=1"
 export PGPASSWORD=`cat /secrets/db/password`; psql -U dataverse dataverse -h postgres -c "select * from apitoken"
