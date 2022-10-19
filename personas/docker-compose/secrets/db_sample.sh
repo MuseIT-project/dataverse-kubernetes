efbf4894982b29400f3f9d388656507e88986dac
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2022-10-19 19:43:44 +0200

    OAI record update

--- personas/docker-compose/secrets/db_sample.sh
+++ personas/docker-compose/secrets/db_sample.sh
@@ -2,3 +2,4 @@ export PGPASSWORD=`cat /secrets/db/password`; psql -U dataverse dataverse -h pos
 export PGPASSWORD=`cat /secrets/db/password`; psql -U dataverse dataverse -h postgres -c "update apitoken set tokenstring='5cbd0d29-6174-4c44-9285-f865c9820eba' where id=1"
 export PGPASSWORD=`cat /secrets/db/password`; psql -U dataverse dataverse -h postgres -c "update authenticateduser set superuser=True where id=1"
 export PGPASSWORD=`cat /secrets/db/password`; psql -U dataverse dataverse -h postgres -c "select * from apitoken"
+export PGPASSWORD=`cat /secrets/db/password`; psql -U dataverse dataverse -h postgres -c "insert into oairecord (globalid, lastupdatetime, removed) values ('doi:10.34622/datarepositorium/SGXCQO', '2022-10-18 15:21:59.625', False)"
